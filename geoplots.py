07ec06534855cbf5be12c17a9c527f17a4ba2f5c
Eduardo Nzinga <eduardonzinga111@hotmail.com>
2018-08-22 12:57:48 -0300

    First map built

--- geoplots.py
+++ geoplots.py
@@ -12,6 +12,28 @@ except ImportError:
     
 print (np.__version__)
 
-import plotly as py
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+init_notebook_mode(connected=True)
+
 import cufflinks as cf
+import pandas as pd
+
+data = dict(type = 'choropleth', 
+            locations =['AZ', 'CA', 'NY'], 
+            locationmode = 'USA-states',
+            colorscale = 'Portland',
+            text = ['Estado1', 'Estado2', 'Estado3'],
+            z = [1.0, 2.0, 3.0],
+            colorbar = {'title': 'Título'})
+
+layout = dict(geo = {'scope': 'usa'})
+
+choromap = go.Figure(data=[data], layout = layout)
+
+plot(choromap)
+
+
 

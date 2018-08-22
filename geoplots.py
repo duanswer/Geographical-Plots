8d0f65ba18bb2ec2c4434788e7bc6337648942dd
Eduardo Nzinga <eduardonzinga111@hotmail.com>
2018-08-22 09:07:43 -0300

    First commit is (only libs)

---
+++ geoplots.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 10:09:38 2018
+
+@author: Eduardo Answer <eduardonzinga111@hotmail.com>
+"""
+
+try:
+    import numpy as np
+except ImportError:
+    print("Numpy is not installed")
+    
+print (np.__version__)
+
+import plotly as py
+import cufflinks as cf
+
